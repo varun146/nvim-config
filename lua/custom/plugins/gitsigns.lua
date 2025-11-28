195ba47c2be1a246b9e7c1da96c3708729e4bb22
Varun Garg <varun_garg@epam.comsdfsdfs>
2025-11-28 19:13:36 +0530

    Initial Commit

---
+++ lua/custom/plugins/gitsigns.lua
@@ -0,0 +1,9 @@
+return {
+  'lewis6991/gitsigns.nvim',
+  opts = {
+    current_line_blame = true, -- Show blame for the current line
+    current_line_blame_opts = {
+      delay = 500,
+    },
+  },
+}
